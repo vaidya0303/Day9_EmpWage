a4acdf49bc4422024d97ca757a01c33a54adc5e3
vaidya0303 <vaidya0304@gmail.com>
2022-10-09 16:10:05 +0530

    switch case

--- Day9EmpWage.java
+++ Day9EmpWage.java
@@ -1,31 +1,31 @@
-
-public class Day9EmpWage {
+public class Day9EmpWage{
     public static void main(String args[]) {
-
-        // constant
-        final int PART_TIME = 1;  //added part time employee
+        //constatnt
+        final int PART_TIME = 1;
         final int FULL_TIME = 2;
         final int WAGE_PER_HR = 20;
         int empType = (int) (Math.random() * 100) % 3;
-        int workingHours = 0;
+        int workingHours =0;
+        
         /*
-        * if else condition is used for employee
-        * is present for part time or full time
-        */
-        if (empType == FULL_TIME)
-        {
-            System.out.println("Employee is Present Full time");
-            workingHours = 8;
-        } else if (empType == PART_TIME)
-        {
-            System.out.println("Employee is Present Part time");
-            workingHours = 4;
-        } else
-        {
-            System.out.println("Employee is Absent");
+        using switch case for employee present or absent.if employee present in full tm then working hr is 8,
+        if employee absent in part time then working hr is 4.
+         */
+        
+        switch (empType) {
+            case FULL_TIME:
+                System.out.println("Employee is Present Full time");  //condition 1
+                workingHours = 8;
+                break;
+            case PART_TIME:
+                System.out.println("Employee is Present Part time");  //condition 2
+                workingHours = 4;
+                break;
+                
+            default:
+                System.out.println("Employee is Absent");  // if case 1 and case 2 both are fail then default condition run
         }
-
-        // calculate daily employee wage.
+        //calculate daily wage
         int wage = workingHours * WAGE_PER_HR;
         System.out.println("Employee Daily Wage is " + wage);
     }
