4458a702591d5701a5c37b885209d03888bf951b
vaidya0303 <vaidya0304@gmail.com>
2022-10-09 16:03:59 +0530

    daily wage calculation

--- Day9EmpWage.java
+++ Day9EmpWage.java
@@ -1,19 +1,23 @@
 public class Day9EmpWage{
+    public static void main(String args[]){
     
-    public static void main(String args[]) {
-        
+        //constant
         final int FULL_TIME = 1;
-        
+        final int WAGE_PER_HR = 20;
         int empType = (int) (Math.random() * 100) % 2;
-        
+        int workingHours = 0;
+        /*
+        if condition is true then employee is present otherwise absent
+         */
         if (empType == FULL_TIME)
         {
             System.out.println("Employee is Present");
-
+            workingHours = 8;
         } else
         {
             System.out.println("Employee is Absent");
-
         }
+        int wage = workingHours * WAGE_PER_HR;
+        System.out.println("Employee Daily Wage is " + wage);
     }
 }
