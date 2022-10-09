d20bdeebda3665d8e51802acd872a866b553af0b
vaidya0303 <vaidya0304@gmail.com>
2022-10-09 16:20:54 +0530

    refactor code to compute employee wage

--- Day9EmpWage.java
+++ Day9EmpWage.java
@@ -1,25 +1,27 @@
 public class Day9EmpWage {
-    public static void main(String args[]) {
+    public static void calculateTotalWage() {
         //constant
         final int PART_TIME = 1;
         final int FULL_TIME = 2;
         final int WAGE_PER_HR = 20;
         final int MAX_WORKING_DAYS = 20;
-        final int MAX_WORKING_HRS = 100;// adding max working hr for month
+        final int MAX_WORKING_HRS = 100;
 
         int totalWage = 0;
         int workingHrs = 0;
         System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
-
         /*
-         using for loop for day, workinghrs, wage, and totalworkinghrs
+        using for loop check condition and print days ,workinghrs, wage ,totaal working hrs in month.
          */
         for (int day = 1, totalWorkingHrs = 0; day <= MAX_WORKING_DAYS
-                && totalWorkingHrs < MAX_WORKING_HRS; day++, totalWorkingHrs += workingHrs) {
+                && totalWorkingHrs < MAX_WORKING_HRS; day++, totalWorkingHrs += workingHrs)
+        {
 
             int empType = (int) (Math.random() * 100) % 3;
-            //using switch case for full time and part time working hrs
 
+            /*
+            using switch case
+             */
             switch (empType)
             {
                 case FULL_TIME:
@@ -32,12 +34,17 @@ public class Day9EmpWage {
                     workingHrs = 0;
                     break;
             }
-            //calculate daily empwage
+            //calculate daily wage
             int wage = workingHrs * WAGE_PER_HR;
-            totalWage += wage;//monthly emwage
+            totalWage += wage;// monthly wage
             System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
 
         }
         System.out.println("Total wage for a month is " + totalWage);
+    }
+
+    public static void main(String args[])
+    {
+        calculateTotalWage();
     }
 }
