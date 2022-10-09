f54204f75e1ff7484a8c2dc8e906a938a8edf856
vaidya0303 <vaidya0304@gmail.com>
2022-10-09 16:07:05 +0530

    part time wage

--- Day9EmpWage.java
+++ Day9EmpWage.java
@@ -1,22 +1,31 @@
-public class Day9EmpWage{
-    public static void main(String args[]){
-    
-        //constant
-        final int FULL_TIME = 1;
+
+public class Day9EmpWage {
+    public static void main(String args[]) {
+
+        // constant
+        final int PART_TIME = 1;  //added part time employee
+        final int FULL_TIME = 2;
         final int WAGE_PER_HR = 20;
-        int empType = (int) (Math.random() * 100) % 2;
+        int empType = (int) (Math.random() * 100) % 3;
         int workingHours = 0;
         /*
-        if condition is true then employee is present otherwise absent
-         */
+        * if else condition is used for employee
+        * is present for part time or full time
+        */
         if (empType == FULL_TIME)
         {
-            System.out.println("Employee is Present");
+            System.out.println("Employee is Present Full time");
             workingHours = 8;
+        } else if (empType == PART_TIME)
+        {
+            System.out.println("Employee is Present Part time");
+            workingHours = 4;
         } else
         {
             System.out.println("Employee is Absent");
         }
+
+        // calculate daily employee wage.
         int wage = workingHours * WAGE_PER_HR;
         System.out.println("Employee Daily Wage is " + wage);
     }
