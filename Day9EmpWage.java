8dc2d022095c1bcb934360f986f7e1dcc15ffe4f
vaidya0303 <vaidya0304@gmail.com>
2022-10-09 15:57:10 +0530

    present or absent

---
+++ Day9EmpWage.java
@@ -0,0 +1,19 @@
+public class Day9EmpWage{
+    
+    public static void main(String args[]) {
+        
+        final int FULL_TIME = 1;
+        
+        int empType = (int) (Math.random() * 100) % 2;
+        
+        if (empType == FULL_TIME)
+        {
+            System.out.println("Employee is Present");
+
+        } else
+        {
+            System.out.println("Employee is Absent");
+
+        }
+    }
+}
