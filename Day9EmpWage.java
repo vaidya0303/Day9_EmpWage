fa4711930afdc9e10ab0f838fac8d759f4b6dc7d
vaidya0303 <vaidya0304@gmail.com>
2022-10-09 16:12:52 +0530

    monthly wage

--- Day9EmpWage.java
+++ Day9EmpWage.java
@@ -1,32 +1,39 @@
 public class Day9EmpWage{
+
     public static void main(String args[]) {
-        //constatnt
+
+        // constant
         final int PART_TIME = 1;
         final int FULL_TIME = 2;
         final int WAGE_PER_HR = 20;
-        int empType = (int) (Math.random() * 100) % 3;
-        int workingHours =0;
-        
+        final int WORKING_DAYS = 20;
+
+        int totalWage = 0;
         /*
-        using switch case for employee present or absent.if employee present in full tm then working hr is 8,
-        if employee absent in part time then working hr is 4.
+              using for loop for calculate monthly wage
          */
-        
-        switch (empType) {
-            case FULL_TIME:
-                System.out.println("Employee is Present Full time");  //condition 1
-                workingHours = 8;
-                break;
-            case PART_TIME:
-                System.out.println("Employee is Present Part time");  //condition 2
-                workingHours = 4;
-                break;
-                
-            default:
-                System.out.println("Employee is Absent");  // if case 1 and case 2 both are fail then default condition run
+        for (int day = 1; day <= WORKING_DAYS; day++) {
+            int empType = (int) (Math.random() * 100) % 3;
+            int workingHours =0;
+            
+            /*
+            using switch case 
+             */
+            switch (empType)
+            {
+                case FULL_TIME:
+                    workingHours = 8;
+                    break;
+                case PART_TIME:
+                    workingHours = 4;
+                    break;
+                default:
+            }
+            //calculate daily empwage
+            int wage = workingHours * WAGE_PER_HR;
+            System.out.println("Day " + day + " wage is:" + wage);
+            totalWage += wage; //monthly wage
         }
-        //calculate daily wage
-        int wage = workingHours * WAGE_PER_HR;
-        System.out.println("Employee Daily Wage is " + wage);
+        System.out.println("Total wage for a month is " + totalWage);
     }
 }
